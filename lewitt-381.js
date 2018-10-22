d861766a5bfad1e4c7b7be1363f329ee5e185b1f
tony <43459464+tonyidarraga@users.noreply.github.com>
2018-10-21 20:33:53 -0700

    Create lewitt-381.js

---
+++ lewitt-381.js
@@ -0,0 +1,24 @@
+function setup() {
+	createCanvas(400, 400);
+}
+
+function draw() {
+	
+	noStroke();
+	
+	//yellow
+	fill(236, 227, 94);
+	rect(200, 200, 200, -200);
+
+	//silver
+	fill(192, 192, 192);
+	rect(0, 0, 200, 200);
+
+	//magenta
+	fill(255, 76, 133);
+	rect(0, 200, 200, 200);
+
+	//blue
+	fill(40, 183, 255);
+	rect(200, 200, 200, 200);
+}
