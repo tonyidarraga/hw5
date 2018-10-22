0821dd943f5f5e78f06c7df3c63a1f109bab3d6d
tony <43459464+tonyidarraga@users.noreply.github.com>
2018-10-21 21:50:53 -0700

    Update lewitt-630.js

--- lewitt-630.js
+++ lewitt-630.js
@@ -1,5 +1,5 @@
 function setup() {
-	createCanvas(400, 200);
+	createCanvas(380, 200);
 }
 
 function draw() {
@@ -10,7 +10,18 @@ function draw() {
 	line(0, 60, 400, 60);
 	line(0, 100, 400, 100);
 
-	for (var x = 0; x <= width; x = x + 50); {
-		line(x, 100, x, 400);
-	}
+	line(0, 100, 0, 400);
+	line(50, 100, 50, 400);
+	line(100, 100, 100, 400);
+	line(150, 100, 150, 400);
+	line(200, 100, 200, 400);
+	line(250, 100, 250, 400);
+	line(300, 100, 300, 400);
+	line(350, 100, 350, 400);
+	line(400, 100, 400, 400);
+	
+	// i tried to use this loop:
+	// but it didn't work:
+	//for (var x = 0; x <= width; x = x + 50); {
+	//line(0, 100, 0, 400);
 }
